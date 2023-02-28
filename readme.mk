ebee74fea600c4a3d3db1544dff2138b2c93ff4e
SR <srehaag@osgoode.yorku.ca>
2023-02-28 08:59:58 -0500

    added readme, cleaned repo

---
+++ readme.mk
@@ -0,0 +1,18 @@
+### Repo with notebook to gather and visualize data about all Osgoode Hall Law School faculty member research expertise
+
+Requirements:
+    
+    !pip install pandas
+    !pip install bs4
+    !pip install openai
+    !pip install requests
+    !pip install wordcloud
+
+Also requires [OpenAI account](https://openai.com/) and [OpenAI API key](https://platform.openai.com/account/api-keys). The key must be placed in a text file and pointed 
+to in the setup cell (open_ai_inpath) below. Alternatively, store API key in os environment variable and use the commented out snippet in setup below.
+
+To run, open 'srape_visualize_notebooke.ipynb'.
+
+License: [CC BY-NC 3.0](https://creativecommons.org/licenses/by-nc/3.0/)
+
+Citation: Sean Rehaag, "Notebook to gather and visualize data about all Osgoode Hall Law School faculty member research expertise" (28 February 2023) online: https://github.com/Refugee-Law-Lab/osgoode_research_expertise.
